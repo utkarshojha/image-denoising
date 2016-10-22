eff299e6404ddcfaadd5a54cf3815b9230e6b281
utkarsh2254 <utkarsh2254@gmail.com>
2016-10-23 02:49:43 +0530

    Update band1_done.py

--- band1_done.py
+++ band1_done.py
@@ -1,35 +1,3 @@
-"""
- This tutorial introduces stacked denoising auto-encoders (SdA) using Theano.
-
- Denoising autoencoders are the building blocks for SdA.
- They are based on auto-encoders as the ones used in Bengio et al. 2007.
- An autoencoder takes an input x and first maps it to a hidden representation
- y = f_{\theta}(x) = s(Wx+b), parameterized by \theta={W,b}. The resulting
- latent representation y is then mapped back to a "reconstructed" vector
- z \in [0,1]^d in input space z = g_{\theta'}(y) = s(W'y + b').  The weight
- matrix W' can optionally be constrained such that W' = W^T, in which case
- the autoencoder is said to have tied weights. The network is trained such
- that to minimize the reconstruction error (the error between x and z).
-
- For the denosing autoencoder, during training, first x is corrupted into
- \tilde{x}, where \tilde{x} is a partially destroyed version of x by means
- of a stochastic mapping. Afterwards y is computed as before (using
- \tilde{x}), y = s(W\tilde{x} + b) and z as s(W'y + b'). The reconstruction
- error is now measured between z and the uncorrupted input x, which is
- computed as the cross-entropy :
-      - \sum_{k=1}^d[ x_k \log z_k + (1-x_k) \log( 1-z_k)]
-
-
- References :
-   - P. Vincent, H. Larochelle, Y. Bengio, P.A. Manzagol: Extracting and
-   Composing Robust Features with Denoising Autoencoders, ICML'08, 1096-1103,
-   2008
-   - Y. Bengio, P. Lamblin, D. Popovici, H. Larochelle: Greedy Layer-Wise
-   Training of Deep Networks, Advances in Neural Information Processing
-   Systems 19, 2007
-
-"""
-
 from __future__ import print_function
 
 import os
@@ -47,18 +15,8 @@ from mlp import HiddenLayer
 from dA import dA
 
 
-# start-snippet-1
 class SdA(object):
-    """Stacked denoising auto-encoder class (SdA)
-
-    A stacked denoising autoencoder model is obtained by stacking several
-    dAs. The hidden layer of the dA at layer `i` becomes the input of
-    the dA at layer `i+1`. The first layer dA gets as input the input of
-    the SdA, and the hidden layer of the last dA represents the output.
-    Note that after pretraining, the SdA is dealt with as a normal MLP,
-    the dAs are only used to initialize the weights.
-    """
-
+  
     def __init__(
         self,
         numpy_rng,
@@ -68,31 +26,7 @@ class SdA(object):
         n_outs=10,
         corruption_levels=[0.1, 0.1]
     ):
-        """ This class is made to support a variable number of layers.
-
-        :type numpy_rng: numpy.random.RandomState
-        :param numpy_rng: numpy random number generator used to draw initial
-                    weights
-
-        :type theano_rng: theano.tensor.shared_randomstreams.RandomStreams
-        :param theano_rng: Theano random generator; if None is given one is
-                           generated based on a seed drawn from `rng`
-
-        :type n_ins: int
-        :param n_ins: dimension of the input to the sdA
-
-        :type hidden_layers_sizes: list of ints
-        :param hidden_layers_sizes: intermediate layers size, must contain
-                               at least one value
-
-        :type n_outs: int
-        :param n_outs: dimension of the output of the network
-
-        :type corruption_levels: list of float
-        :param corruption_levels: amount of corruption to use for each
-                                  layer
-        """
-
+       
         self.sigmoid_layers = []
         self.dA_layers = []
         self.params = []
@@ -106,32 +40,16 @@ class SdA(object):
         self.x = T.matrix('x')  # the data is presented as rasterized images
         self.y = T.matrix('y')  # the labels are presented as 1D vector of
                                  # [int] labels
-        # end-snippet-1
-
-        # The SdA is an MLP, for which all weights of intermediate layers
-        # are shared with a different denoising autoencoders
-        # We will first construct the SdA as a deep multilayer perceptron,
-        # and when constructing each sigmoidal layer we also construct a
-        # denoising autoencoder that shares weights with that layer
-        # During pretraining we will train these autoencoders (which will
-        # lead to chainging the weights of the MLP as well)
-        # During finetunining we will finish training the SdA by doing
-        # stochastich gradient descent on the MLP
-
-        # start-snippet-2
+      
         for i in range(self.n_layers):
-            # construct the sigmoidal layer
-
-            # the size of the input is either the number of hidden units of
+           
             # the layer below or the input size if we are on the first layer
             if i == 0:
                 input_size = n_ins
             else:
                 input_size = hidden_layers_sizes[i - 1]
 
-            # the input to this layer is either the activation of the hidden
-            # layer below or the input of the SdA if you are on the first
-            # layer
+
             if i == 0:
                 layer_input = self.x
             else:
@@ -161,8 +79,8 @@ class SdA(object):
                           W=sigmoid_layer.W,
                           bhid=sigmoid_layer.b)
             self.dA_layers.append(dA_layer)
-        # end-snippet-2
-        # We now need to add a logistic layer on top of the MLP
+     
+        # I now need to add a logistic layer on top of the MLP
         self.logLayer = LogisticRegression(
             input=self.sigmoid_layers[-1].output,
             n_in=hidden_layers_sizes[-1],
@@ -170,35 +88,13 @@ class SdA(object):
         )
 
         self.params.extend(self.logLayer.params)
-        # construct a function that implements one step of finetunining
-
-        # compute the cost for second phase of training,
-        # defined as the negative log likelihood
+ 
         self.finetune_cost = self.logLayer.negative_log_likelihood(self.y)
-        # compute the gradients with respect to the model parameters
-        # symbolic variable that points to the number of errors made on the
-        # minibatch given by self.x and self.y
+   
         self.errors = self.logLayer.errors(self.y)
 
     def pretraining_functions(self, train_set_x, batch_size):
-        ''' Generates a list of functions, each of them implementing one
-        step in trainnig the dA corresponding to the layer with same index.
-        The function will require as input the minibatch index, and to train
-        a dA you just need to iterate, calling the corresponding function on
-        all minibatch indexes.
-
-        :type train_set_x: theano.tensor.TensorType
-        :param train_set_x: Shared variable that contains all datapoints used
-                            for training the dA
-
-        :type batch_size: int
-        :param batch_size: size of a [mini]batch
-
-        :type learning_rate: float
-        :param learning_rate: learning rate used during training for any of
-                              the dA layers
-        '''
-
+      
         # index to a [mini]batch
         index = T.lscalar('index')  # index to a minibatch
         corruption_level = T.scalar('corruption')  # % of corruption to use
@@ -232,24 +128,7 @@ class SdA(object):
         return pretrain_fns
 
     def build_finetune_functions(self, datasets, batch_size):
-        '''Generates a function `train` that implements one step of
-        finetuning, a function `validate` that computes the error on
-        a batch from the validation set, and a function `test` that
-        computes the error on a batch from the testing set
-
-        :type datasets: list of pairs of theano.tensor.TensorType
-        :param datasets: It is a list that contain all the datasets;
-                         the has to contain three pairs, `train`,
-                         `valid`, `test` in this order, where each pair
-                         is formed of two Theano variables, one for the
-                         datapoints, the other for the labels
-
-        :type batch_size: int
-        :param batch_size: size of a minibatch
-
-        :type learning_rate: float
-        :param learning_rate: learning rate used during finetune stage
-        '''
+   
 
         (train_set_x, train_set_y) = datasets[0]
         (valid_set_x, valid_set_y) = datasets[1]
@@ -334,40 +213,20 @@ class SdA(object):
 
 def test_SdA(finetune_lr=0.2, pretraining_epochs=30,
              pretrain_lr=0.1, training_epochs=180,
-             dataset='mnist.pkl.gz', batch_size=30):
-    """
-    Demonstrates how to train and test a stochastic denoising autoencoder.
-
-    This is demonstrated on MNIST.
-
-    :type learning_rate: float
-    :param learning_rate: learning rate used in the finetune stage
-    (factor for the stochastic gradient)
-
-    :type pretraining_epochs: int
-    :param pretraining_epochs: number of epoch to do pretraining
-
-    :type pretrain_lr: float
-    :param pretrain_lr: learning rate to be used during pre-training
-
-    :type n_iter: int
-    :param n_iter: maximal number of iterations ot run the optimizer
-
-    :type dataset: string
-    :param dataset: path the the pickled dataset
-
-    """
+             dataset=None, batch_size=30):
+  
 
 
 #    ##########################################################    
     #a = [100907,103342,105542,112311,114345]
     numpy_rng = numpy.random.RandomState(10)
     npix=4000
+    #setting the dimension of the patch size	
     blockx=26
     blocky=26
-    n_training_ex=100000
-    n_valid_ex=2000
-    n_test_ex=2000  
+    n_training_ex=100000 #number of random patches to be sampled from each image in the training set
+    n_valid_ex=2000  #number of random patches sampled from the validation set
+    n_test_ex=2000  #number of random patches sampled from the test set
     print('Loading the image data')
     def read_filename(string , n , n_ex ):
         s = string
@@ -481,7 +340,9 @@ def test_SdA(finetune_lr=0.2, pretraining_epochs=30,
         try_batch = (try_batch.transpose() / dif).transpose()   
         return trx_batch , try_batch 
     
-
+#Following are the list of paths to the images of training set which I used when I worked at my computer
+#My training set consisted of 12 multispectral images (I know it will sound too less considering we're dealing with deep learning models, but each of those images has dimension 4000 x 80000.
+# So I sampled 1000000 random patches (described later), resulting in my training set consisting 12000000 noisy and desnoied patches of size 26 x 26. 
     train_string_set1=['/AKASHDP_DATA3/ankur/train/A1-NAO-25-FEB-2016-100907-R2-1_b1.rad_boost']
     train_string_set2=['/AKASHDP_DATA3/ankur/train/A1-NAO-25-FEB-2016-103342-R3-1_b1.rad_boost']
     train_string_set3=['/AKASHDP_DATA3/ankur/train/A1-NAO-25-FEB-2016-105542-R4-1_b1.rad_boost']
@@ -495,7 +356,7 @@ def test_SdA(finetune_lr=0.2, pretraining_epochs=30,
     train_string_set11=['/AKASHDP_DATA3/ankur/train/A1-NAO-25-FEB-2016-120945-R7-0_b1.rad_boost']
     train_string_set12=['/AKASHDP_DATA3/ankur/train/A1-NAO-25-FEB-2016-122040-R17-0_b1.rad_boost']
   
-    
+#Forming the training set    
     trx_batch,try_batch=read_filename(train_string_set1,1,n_training_ex)    
     tra_batch,trb_batch=read_filename(train_string_set2,1,n_training_ex)
     trx_batch = numpy.vstack([trx_batch,tra_batch])
